b92dc1e850b93e67f3d797a39f0d40bd05a81e86
Ryan Dulko <ryandulko458@gmail.com>
2023-11-15 11:54:15 -0300

    adcionando produtos ao carrinho

--- cypress/e2e/pre-cadastro.spec.cy.js
+++ cypress/e2e/pre-cadastro.spec.cy.js
@@ -2,23 +2,26 @@
 const { faker } = require('@faker-js/faker');
 
 describe('Funcionalidade Pre cadastro', () => {
-    
+
     beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
     });
 
     it('Deve completar o pre cadastro com sucesso', () => {
-        
-        cy.get('#reg_email').type(faker.internet.email())
+        let emailfaker = faker.internet.email()
+        let nomefaker = faker.name.firstName()
+        let sobrenomefaker = faker.name.lastName()
+
+        cy.get('#reg_email').type(emailfaker)
         cy.get('#reg_password').type('teste_ryan22')
         cy.get(':nth-child(4) > .button').click()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type(faker.name.firstName())
-        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('#account_first_name').type(nomefaker)
+        cy.get('#account_last_name').type(sobrenomefaker)
         cy.get('.woocommerce-Button').click()
 
-        cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso')
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso')
     });
 
 
---
+++ cypress/e2e/produtos.spec.cy.js
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+
+
+describe('Funcionalidade Pagina de produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+    
+    it('Deve selecionar um produto da lista', () => {
+        
+        cy.get('.product-block')
+        //.first()
+        //.last()
+        //.eq(3)
+        .contains('Chaz Kangeroo Hoodie')
+        .click()
+
+    });
+
+    it.only('Deve adcionar um produto ao carrinho', () => {
+        var quantidade = 3
+
+        cy.get('.product-block')
+        .eq(4).click()
+        cy.get('.button-variable-item-33').click()
+        cy.get('.button-variable-item-Black').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+       
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain' , quantidade)
+        cy.get('.woocommerce-message').should('contain' , quantidade + ' × “Caesar Warm-Up Pant” foram adicionados no seu carrinho.')
+    });
+
+
+});
