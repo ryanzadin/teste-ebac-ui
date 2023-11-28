8c250a91b4986fe2aa9987d920d2d00ad7b62d45
Ryan Dulko <ryandulko458@gmail.com>
2023-11-27 21:08:59 -0300

    endereco de faturamento

--- cypress/e2e/endereco.spec.cy.js
+++ cypress/e2e/endereco.spec.cy.js
@@ -1,18 +1,18 @@
 /// <reference types="cypress" />
+import enderecoPage from '../support/page-objects/endereco.page'
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
         cy.visit('minha conta')
         cy.fixture('perfil').then(dados => {
             cy.login(dados.usuario, dados.senha)
+            
         })
     });
 
-    it('Deve fazer o cadastro do faturamento com sucesso', () => {
-        cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
-        cy.get(':nth-child(1) > .title > .edit').click()
-
-
+    it.only('Deve fazer o cadastro do faturamento com sucesso', () => {
+       enderecoPage.editarEnderecoFaturamento('Ryanir', 'Dulko', 'temperao', 'brasil', 'rua doutor augusto', '543', 'sao paulo', 'sao paulo', '03496-101', '11990240453', 'Ryanzzaz@teste.com' )
+       cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
 
     });
 });
