e7f59392f619f805d72caa3d0a27c77aaf7f7775
Ryan Dulko <ryandulko458@gmail.com>
2023-11-27 22:04:30 -0300

    arquivos de dados

--- cypress/e2e/endereco.spec.cy.js
+++ cypress/e2e/endereco.spec.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 import enderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require ('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -10,9 +11,28 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
         })
     });
 
-    it.only('Deve fazer o cadastro do faturamento com sucesso', () => {
+    it('Deve fazer o cadastro do faturamento com sucesso', () => {
        enderecoPage.editarEnderecoFaturamento('Ryanir', 'Dulko', 'temperao', 'brasil', 'rua doutor augusto', '543', 'sao paulo', 'sao paulo', '03496-101', '11990240453', 'Ryanzzaz@teste.com' )
        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
 
     });
+
+    it.only('Deve fazer o cadastro do faturamento com sucesso - usando arquivos de dados', () => {
+        enderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[0].nome,
+            dadosEndereco[0].sobrenome,
+            dadosEndereco[0].empressa,
+            dadosEndereco[0].pais,
+            dadosEndereco[0].rua,
+            dadosEndereco[0].numero,
+            dadosEndereco[0].cidade,
+            dadosEndereco[0].estado,
+            dadosEndereco[0].CEP,
+            dadosEndereco[0].telefone,
+            dadosEndereco[0].email
+            )
+
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+ 
+     });
 });
